a1e1fc01be348e2c00b23f02e53ba792454d97b4
Andrew Park <atpark333@gmail.com>
2022-04-04 21:25:03 +0900

    Add exercise 3 solution

---
+++ 01-basics/ex3.rb
@@ -0,0 +1,11 @@
+movies = {
+  ivans_childhood: 1962,
+  andrei_rublev: 1966,
+  solaris: 1972,
+  mirror: 1975,
+  stalker: 1979,
+  nostalghia: 1983,
+  the_sacrifice: 1986
+}
+
+movies.each_value { |year| puts year }
