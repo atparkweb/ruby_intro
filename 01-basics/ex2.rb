9e24156fad42a510d7db711203fb0e53572f4a16
Andrew Park <atpark333@gmail.com>
2022-04-04 21:18:31 +0900

    Add magic comment and check args

--- 01-basics/ex2.rb
+++ 01-basics/ex2.rb
@@ -1,14 +1,21 @@
+# frozen_string_literal: true
+
+# Use the modulo operator, division, or a combination of both to take a 4 digit number
+# and find the digit in the: 1) thousands place 2) hundreds place 3) tens place 4) ones place
 unless ARGV.length == 1
-  puts "Usage: ruby ex2.rb [integer]"
+  puts 'Usage: ruby ex2.rb <integer>'
   exit
 end
 
 number = ARGV[0].to_i
 
+raise 'Number must be less than 10,000' if number > 9999
+
+# Add spaces after key to align output
 def pad_key(key)
-  result = '';
-  (10 - key.length).times { result << ' ' }
-  result
+  result = []
+  (10 - key.length).times { result.push(' ') }
+  result.join('')
 end
 
 {
