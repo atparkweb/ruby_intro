52eac9f2009e613e14eb4db739f0357f3274c657
Andrew Park <atpark333@gmail.com>
2022-04-15 10:31:44 +0900

    Reformat exercises to be in a single method

--- 09-exercises/solutions.rb
+++ 09-exercises/solutions.rb
@@ -3,23 +3,25 @@
 ##
 # Exercise solutions
 module Exercises
-  ##
-  # Use the `each` method of Array to iterate over `[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]`,
-  # and print out each value
-  def solution_one
-    (1..10).each { |num| puts num }
-  end
+  def run
+    ## 1
+    # Use the `each` method of Array to iterate over `[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]`,
+    # and print out each value
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    arr.each { |num| puts num }
 
-  ##
-  # Same as above, but only print out values greater than 5.
-  def solution_two
-    (1..10).each { |num| puts num if num > 5 }
-  end
+    ## 2
+    # Same as above, but only print out values greater than 5.
+    arr.each { |num| puts num if num > 5 }
 
-  ##
-  # Now, using the same array from #2, use the `select` method to extract all odd numbers
-  # into a new array.
-  def solution_three
-    (1..10).select(&:odd?)
+    ## 3
+    # Now, using the same array from #2, use the `select` method to extract all odd numbers
+    # into a new array.
+    arr.select { |num| num % 2 != 0 }
+
+    ## 4
+    # Append `11` to the end of the original array. Prepend `0` to the beginning.
+    arr.push(11)
+    arr.unshift(0)
   end
 end
