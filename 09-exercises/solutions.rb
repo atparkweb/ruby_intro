d884a1832de9d52a0c36c0437b392d74ead18ed9
Andrew Park <atpark333@gmail.com>
2022-04-16 20:57:44 +0900

    Solution for exercise 11

--- 09-exercises/solutions.rb
+++ 09-exercises/solutions.rb
@@ -77,5 +77,28 @@ module Exercises
     ## 10
     # Q: Can hash values be arrays? Can you have an array of hashes? (give examples)
     # A:
+
+    # 11
+    # Given the following data structures, write a program that copies the information from the array into the empty
+    # hash that applies to the correct person.
+    contact_data = [
+      ['joe@email.com', '123 Main st.', '555-123-4567'],
+      ['sally@email.com', '404 Not Found Dr.', '123-234-3454']
+    ]
+    contacts = { 'Joe Smith' => {}, 'Sally Johnson' => {} }
+
+    def info_to_map(values, map)
+      map[:email] = values[0]
+      map[:address] = values[1]
+      map[:phone] = values[2]
+    end
+
+    info_to_map(contact_data[0], contacts['Joe Smith'])
+    info_to_map(contact_data[1], contacts['Sally Johnson'])
+    test(contacts,
+         {
+           'Joe Smith' => { email: 'joe@email.com', address: '123 Main st.', phone: '555-123-4567' },
+           'Sally Johnson' => { email: 'sally@email.com', address: '404 Not Found Dr.', phone: '123-234-3454' }
+         })
   end
 end
